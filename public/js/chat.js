719552e17fd47824baa93118598d8f51a9e9b1cc
Ridwan Taiwo <donriddo@gmail.com>
2019-04-07 20:28:04 +0100

    Show number of comments/replies

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,13 +1,19 @@
 $(() => {
     window.currentPictureId = '';
     window.currentCommentId = '';
+    window.currentThreadUrl = '';
 
     $('#commentButton').click((evt) => {
         evt.preventDefault();
+        const text = $('#commentText').val();
+        if (!text) {
+            alert('Please enter some text');
+            return false;
+        }
         $.ajax({
-            url: 'http://localhost:37337/api/picture/comment/',
+            url: currentThreadUrl,
             type: 'POST',
-            data: { text: $('#commentText').val(), picture: currentPictureId },
+            data: { text: text, picture: currentPictureId, comment: currentCommentId },
             beforeSend: function (xhr) {
                 const userToken = localStorage.getItem('userToken');
                 xhr.setRequestHeader('Authorization', "Bearer" + " " + userToken);
@@ -16,7 +22,7 @@ $(() => {
         .done((data) => {
             console.log({data});
             $('#commentText').val('');
-            setPictureThread(currentPictureId);
+            currentThreadUrl.includes('reply') ? setCommentThread(currentCommentId) : setPictureThread(currentPictureId);
         })
         .fail((err) => {
             console.log({err});
@@ -28,6 +34,10 @@ $(() => {
         var fileInput = $('#image');
         console.log({fileInput: fileInput[0].files})
         var file = fileInput[0].files[0];
+        if (!file) {
+            alert('Please upload an image');
+            return false;
+        }
         var formData = new FormData();
         formData.append('image', file);
         $.ajax({
@@ -51,6 +61,7 @@ $(() => {
 
     window.setPictureThread = async (id) => {
         currentPictureId = id;
+        currentThreadUrl = 'http://localhost:37337/api/picture/comment/';
         let picture = await $.ajax({
             url: `http://localhost:37337/api/picture/${currentPictureId}`,
             beforeSend: function (xhr) {
@@ -72,7 +83,8 @@ $(() => {
                 <div class="picturePost">
                     <div class="picTitle">${user.email}</div>
                     <p>${comment.text}</p>
-                    <button onclick="setCommentThread(\'${comment._id}\')" class="photobutton">Reply</button>
+                    <div class="picTitle">${comment.replies.length} replies</div>
+                    <button onclick="setCommentThread(\'${comment._id}\')" class="photobutton">Reply or View Replies</button>
                 </div>
             `);
         });
@@ -81,6 +93,7 @@ $(() => {
 
     window.setCommentThread = async (id) => {
         currentCommentId = id;
+        currentThreadUrl = 'http://localhost:37337/api/comment/reply';
         let comment = await $.ajax({
             url: `http://localhost:37337/api/comment/${currentCommentId}`,
             beforeSend: function (xhr) {
@@ -102,7 +115,8 @@ $(() => {
                 <div class="picturePost">
                     <div class="picTitle">${user.email}</div>
                     <p>${reply.text}</p>
-                    <button onclick="setCommentThread(\'${reply._id}\')" class="photobutton">Reply</button>
+                    <div class="picTitle">${reply.replies.length} replies</div>
+                    <button onclick="setCommentThread(\'${reply._id}\')" class="photobutton">Reply or view replies</button>
                 </div>
             `);
         });
@@ -123,7 +137,8 @@ $(() => {
                     <div class="picturePost">
                         <div class="picTitle">${picture.user.email}</div>
                         <image class="picImage" src=${picture.url} />
-                        <button onclick="setPictureThread(\'${picture._id}\')" class="photobutton">Post a comment</button>
+                        <div class="picTitle">${picture.comments.length} comments</div>
+                        <button onclick="setPictureThread(\'${picture._id}\')" class="photobutton">Comment or view comments</button>
                     </div>
                 `);
             });
